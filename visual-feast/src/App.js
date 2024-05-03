13051049b04d40ee0170352a96ea0204ed7de0fb
karthikeyan <karthikn2895@gmail.com>
2024-05-04 00:02:09 +0530

    setup

---
+++ visual-feast/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from "react"
+import ImageUploader from './component/imageUploader';
+
+function App() {
+  
+  return (
+    <div className="App">
+        </div>
+  );
+}
+
+export default App;
